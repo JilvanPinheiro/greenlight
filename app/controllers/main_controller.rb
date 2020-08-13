a3e8f5f36cf91599f397334be2e944aa562a8a3f
Ubuntu <ubuntu@ip-172-31-35-192.sa-east-1.compute.internal>
2020-08-13 00:53:41 +0000

    Change address

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,39 +3,34 @@ class MainController < ApplicationController
   require "net/http"
   require "uri"
   require 'json'
+  require 'nokogiri'
+
+
   
   # GET /
   def index
     # Store invite token
     session[:invite_token] = params[:invite_token] if params[:invite_token] && invite_registration
   end
-
+  
   # POST /validate
   def validate
     @cpf = Cpf.new(number: main_params[:number])
-
+    
     @cpf[:number].gsub(".", "")
     @cpf[:number].gsub("-", "")
-
-
-    uri = URI.parse("https://arsoluti.acsoluti.com.br/site/idn-psbio-verify")
-    header = {'Content-Type': 'text/json'}
     
-    # Create the HTTP objects
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    request = Net::HTTP::Post.new(uri.request_uri, header)
-    body = {cpf: @cpf[:number] }
-    request.body = body.to_json
     
-    # Send the request
-    response = http.request(request)
-    result = JSON.parse(response.body)
+    uri = URI('https://validacao-interna.validcertificadora.com.br/')
+    res = Net::HTTP.post_form(uri, 'cpf' => @cpf[:number], 'gg' => 'Verificar')
+    document = Nokogiri::HTML(res.body)
+    resultado = document.at('#resultcpf').text
 
-    if result["idnValid"]
+    if resultado.include? "disponível"
       flash[:success] = "O CPF #{@cpf[:number]} pode ser atendido por videoconferência!"
-    else
-      flash[:alert] = "O CPF #{@cpf[:number]} não pode ser atendido por videoconferência!"
+    end
+    if resultado.include? "indisponível"
+        flash[:alert] = "O CPF #{@cpf[:number]} não pode ser atendido por videoconferência!"
     end
     
     return redirect_to "/"
--- config/application.rb
+++ config/application.rb
@@ -130,7 +130,7 @@ module Greenlight
     # DEFAULTS
 
     # Default branding image if the user does not specify one
-    config.branding_image_default = "http://doccloud.com.br/images/logo_2.png"
+    config.branding_image_default = "https://s3-sa-east-1.amazonaws.com/doccloud.com.br/logo_doccloud_com_texto_azul.png"
 
     # Default primary color if the user does not specify one
     config.primary_color_default = "#467fcf"
